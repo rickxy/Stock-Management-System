9f5dda3dc6795cc4fde28aa63117935bb02ecfb8
Ricky <36708000+rickxy@users.noreply.github.com>
2022-09-12 11:22:41 +0300

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,6 @@
 <?php
  $db = mysqli_connect('localhost', 'root', '') or
         die ('Unable to connect. Check your connection parameters.');
-        mysqli_select_db($db, 'sms' ) or die(mysqli_error($db));
+        mysqli_select_db($db, 'scms' ) or die(mysqli_error($db));
         
 ?>
